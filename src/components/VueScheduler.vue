7d2c54d21f9c80e470456d730898c8c8df1f56fa
Denisoed <denisoed@gmail.com>
2019-12-01 15:18:18 +0600

    Added start markup table

--- src/components/VueScheduler.vue
+++ src/components/VueScheduler.vue
@@ -1,7 +1,10 @@
 <template>
   <section class="scheduler">
-    <div class="scheduler_time-column">
-      <div class="scheduler_time-column-row" v-for="(time, i) in timeList" :key="i">{{ time }}</div>
+    <div class="scheduler_column scheduler_column-time">
+      <div class="scheduler_column-row" v-for="(time, i) in timeList" :key="i">{{ time }}</div>
+    </div>
+    <div class="scheduler_column" v-for="(day, i) in dayList" :key="i">
+      <div class="scheduler_column-row" v-for="(time, i) in timeList" :key="i"></div>
     </div>
   </section>
 </template>
@@ -13,11 +16,12 @@ export default {
   name: 'VueScheduler',
   data() {
     return {
+      dayList: [1, 2, 3, 4, 5, 6],
       timeList: []
     };
   },
   created() {
-    this.generateTimeList('07:00', '23:00');
+    this.generateTimeList('07:00', '06:00');
   },
   methods: {
     generateTimeList(start, end) {
@@ -36,3 +40,39 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+  .scheduler {
+    width: auto;
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid #999;
+
+    &_column {
+      width: 100%;
+      border-right: 1px solid #999;
+
+      &:last-child {
+        border: 0;
+      }
+
+      &-row {
+        width: 100%;
+        height: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-bottom: 1px solid #999;
+
+        &:last-child {
+          border: 0;
+        }
+      }
+
+      &-time {
+        min-width: 50px;
+        max-width: 50px;
+      }
+    }
+  }
+</style>
